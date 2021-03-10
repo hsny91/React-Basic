4ff601964594e84abf2367ce34ce58bc012d7289
Husniye <husniyesakmak.91@gmail.com>
2021-03-10 19:50:02 +0100

    BannerImage.js was update

--- website/mywebsite/src/component/BannerImage.js
+++ website/mywebsite/src/component/BannerImage.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function BannerImage() {
     return (
         <div>
-            
+          <img src="../images/1.jpg" alt="banner"/> 
         </div>
     )
 }
