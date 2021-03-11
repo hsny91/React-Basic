0b81116144566d11cab4f5e221c5d181756e6504
Husniye <husniyesakmak.91@gmail.com>
2021-03-11 22:36:09 +0100

    AlbumMaker.js was update

--- website/mywebsite/src/component/BannerImage.js
+++ website/mywebsite/src/component/BannerImage.js
@@ -5,6 +5,7 @@ export default function BannerImage() {
     return (
         <div>
           <img className="banner" src="../images/banner.jpg" alt="banner"/> 
+          <h1>deneme</h1>
         </div>
     )
 }
