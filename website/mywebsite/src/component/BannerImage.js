9d4863e85cba819fc4ba81b02a4b1f45398ea555
Husniye <husniyesakmak.91@gmail.com>
2021-03-12 21:10:33 +0100

    Bannerimage.js was update

--- website/mywebsite/src/component/BannerImage.js
+++ website/mywebsite/src/component/BannerImage.js
@@ -5,7 +5,6 @@ export default function BannerImage() {
     return (
         <div>
           <img className="banner" src="../images/banner.jpg" alt="banner"/> 
-          <h1>deneme</h1>
         </div>
     )
 }
