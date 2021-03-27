65887aa792f53faa5bc0d98c9686562c62f78071
Husniye <husniyesakmak.91@gmail.com>
2021-03-27 21:12:51 +0100

    project was modified

--- Week 02-excell/write.js
+++ Week 02-excell/write.js
@@ -1,9 +1,4 @@
-/**
- * Merhabalar arkadaslar
- EXCEL dosyasindaki datalari okumak ve ekte verilen datalari EXCEL dosyasinin icinde yeni bir sayfa olusturup yazdirma olacaktir. 
- Ödevinizde örnek bir dosyanin okunup içeriğinin ekrana yazdirilmasi ve 
- data.js dosyasindaki datalari EXCEL dosyasina yazdirilmasi gerekmektedir.
- */
+
 
  const xlsx = require("xlsx"); //xlsx modülünü import etmek.
  const JsonSheetUsers = require("./read") //read modülünü import etmek
@@ -13,7 +8,7 @@
  let newBook = xlsx.utils.book_new()
  let newSheetUsers = xlsx.utils.json_to_sheet(JsonSheetUsers) //json yapılarını xlsx e çevirme
  let newSheetOktay = xlsx.utils.json_to_sheet(dataJs) //jsan yapılarını xlsx e çevirme
- xlsx.utils.book_append_sheet(newBook, newSheetOktay, "Oktay") //yeni sheet eklemek
+ xlsx.utils.book_append_sheet(newBook, newSheetOktay, "husniye") //yeni sheet eklemek
  xlsx.utils.book_append_sheet(newBook, newSheetUsers, "Users") //yeni sheet eklemek
  
  xlsx.writeFile(newBook, "./OrnekDosya.xlsx"); //exceli dosyanısını yeniden yazıyor.
