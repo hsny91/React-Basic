deadb4ade0bc2129fb3b56937a3522d37aded2b0
hsny91 <husniyesakmak.91@gmail.com>
2021-04-14 22:50:55 +0200

    project was modified

---
+++ Week-06-API/week06/src/component/ChuckNorris.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import Random from './Random'
+
+export default function ChuckNorris() {
+    const API_RANDOM_URL = `https://api.chucknorris.io/jokes/random`;
+
+    const [word, setWord] = useState([]);
+
+  
+
+    const loadRandomWords = async () =>{
+        const response = await fetch(API_RANDOM_URL);
+        const data = await response.json();
+        setWord(data)
+        console.log(data)
+        console.log(word)
+      }
+
+    return (
+      <div>
+       <Random data ={word} load ={loadRandomWords}/>
+     </div>
+    )
+    
+    
+}
+
